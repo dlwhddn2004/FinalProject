ebb45cb85656bdcf1011f3686a391cc0bbe9dd58
JeaSeok <iai6203@gmail.com>
2020-08-24 20:48:37 +0900

    타임라인 컨트롤러, 서비스, 다오 생성 (재석)

---
+++ src/main/java/kr/or/ddit/timeline/dao/TimelineDaoImpl.java
@@ -0,0 +1,23 @@
+package kr.or.ddit.timeline.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import kr.or.ddit.vo.JoinVO;
+import kr.or.ddit.vo.ProjectVO;
+import kr.or.ddit.vo.SuccessBoardCommentVO;
+import kr.or.ddit.vo.SuccessBoardVO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.lowagie.text.Paragraph;
+
+@Repository
+public class TimelineDaoImpl implements ITimelineDao {
+	
+	@Autowired
+	private SqlMapClient client;
+
+}
