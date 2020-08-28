215a560db1863de5af2cc907af4e6cb6428ff7aa
ktj <kye803112@gmail.com>
2020-08-28 10:49:03 +0900

    vo추가

---
+++ src/main/common/kr/or/ddit/vo/IssueboardCommentVO.java
@@ -0,0 +1,45 @@
+package kr.or.ddit.vo;
+
+public class IssueboardCommentVO {
+	
+	public String issue_no;
+	public String mem_id;
+	public String comment_content; 
+	public String comment_regdate;
+	public String comment_seq;
+	
+	
+	public String getIssue_no() {
+		return issue_no;
+	}
+	public void setIssue_no(String issue_no) {
+		this.issue_no = issue_no;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getComment_content() {
+		return comment_content;
+	}
+	public void setComment_content(String comment_content) {
+		this.comment_content = comment_content;
+	}
+	public String getComment_regdate() {
+		return comment_regdate;
+	}
+	public void setComment_regdate(String comment_regdate) {
+		this.comment_regdate = comment_regdate;
+	}
+	public String getComment_seq() {
+		return comment_seq;
+	}
+	public void setComment_seq(String comment_seq) {
+		this.comment_seq = comment_seq;
+	}
+	
+	
+
+}
