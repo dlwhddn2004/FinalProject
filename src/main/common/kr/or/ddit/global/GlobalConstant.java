8c84cad6d6847a4a7f243fdc7d29598fcecaaaec
dlwhddn2012 <dlwhddn2012@gmail.com>
2020-08-31 14:10:54 +0900

    xZX

--- src/main/common/kr/or/ddit/global/GlobalConstant.java
+++ src/main/common/kr/or/ddit/global/GlobalConstant.java
@@ -1,6 +1,9 @@
 package kr.or.ddit.global;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public interface GlobalConstant {
 	// temp에 files폴더에 파일을 저장
-	public String FILE_PATH = "C:\\temp\\files";
+	public String FILE_PATH = "C:\\Users\\dlwhd\\Desktop\\Server File";
 }
