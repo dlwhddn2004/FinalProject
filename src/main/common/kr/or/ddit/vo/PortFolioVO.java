cfc0de2d9a64e5fffb309258f48e6f8839bc2221
dlwhddn2012 <dlwhddn2012@gmail.com>
2020-09-02 11:24:25 +0900

    포트폴리오 기본 밑바탕 작업!(종우)

---
+++ src/main/common/kr/or/ddit/vo/PortFolioVO.java
@@ -0,0 +1,132 @@
+package kr.or.ddit.vo;
+
+public class PortFolioVO {
+
+	private String portfolio_no;
+	private String mem_id;
+	private String portfolio_categories;
+	private String portfolio_tags;
+	private String portfolio_technologies;
+	private String portfolio_name;
+	private String portfolio_regdate;
+	private String portfolio_design;
+	private String portfolio_useability;
+	private String portfolio_creativity;
+	private String portfolio_content;
+	private String portfolio_develop;
+	private String portfolio_url;
+	private String portfolio_description;
+	private String portfolio_startdate;
+	private String portfolio_enddate;
+	private String portfolio_participationrate;
+	private String portfolio_thumbnailimg;
+	
+	public String getPortfolio_no() {
+		return portfolio_no;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public String getPortfolio_categories() {
+		return portfolio_categories;
+	}
+	public String getPortfolio_tags() {
+		return portfolio_tags;
+	}
+	public String getPortfolio_technologies() {
+		return portfolio_technologies;
+	}
+	public String getPortfolio_name() {
+		return portfolio_name;
+	}
+	public String getPortfolio_regdate() {
+		return portfolio_regdate;
+	}
+	public String getPortfolio_design() {
+		return portfolio_design;
+	}
+	public String getPortfolio_useability() {
+		return portfolio_useability;
+	}
+	public String getPortfolio_creativity() {
+		return portfolio_creativity;
+	}
+	public String getPortfolio_content() {
+		return portfolio_content;
+	}
+	public String getPortfolio_develop() {
+		return portfolio_develop;
+	}
+	public String getPortfolio_url() {
+		return portfolio_url;
+	}
+	public String getPortfolio_description() {
+		return portfolio_description;
+	}
+	public String getPortfolio_startdate() {
+		return portfolio_startdate;
+	}
+	public String getPortfolio_enddate() {
+		return portfolio_enddate;
+	}
+	public String getPortfolio_participationrate() {
+		return portfolio_participationrate;
+	}
+	public String getPortfolio_thumbnailimg() {
+		return portfolio_thumbnailimg;
+	}
+	public void setPortfolio_no(String portfolio_no) {
+		this.portfolio_no = portfolio_no;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public void setPortfolio_categories(String portfolio_categories) {
+		this.portfolio_categories = portfolio_categories;
+	}
+	public void setPortfolio_tags(String portfolio_tags) {
+		this.portfolio_tags = portfolio_tags;
+	}
+	public void setPortfolio_technologies(String portfolio_technologies) {
+		this.portfolio_technologies = portfolio_technologies;
+	}
+	public void setPortfolio_name(String portfolio_name) {
+		this.portfolio_name = portfolio_name;
+	}
+	public void setPortfolio_regdate(String portfolio_regdate) {
+		this.portfolio_regdate = portfolio_regdate;
+	}
+	public void setPortfolio_design(String portfolio_design) {
+		this.portfolio_design = portfolio_design;
+	}
+	public void setPortfolio_useability(String portfolio_useability) {
+		this.portfolio_useability = portfolio_useability;
+	}
+	public void setPortfolio_creativity(String portfolio_creativity) {
+		this.portfolio_creativity = portfolio_creativity;
+	}
+	public void setPortfolio_content(String portfolio_content) {
+		this.portfolio_content = portfolio_content;
+	}
+	public void setPortfolio_develop(String portfolio_develop) {
+		this.portfolio_develop = portfolio_develop;
+	}
+	public void setPortfolio_url(String portfolio_url) {
+		this.portfolio_url = portfolio_url;
+	}
+	public void setPortfolio_description(String portfolio_description) {
+		this.portfolio_description = portfolio_description;
+	}
+	public void setPortfolio_startdate(String portfolio_startdate) {
+		this.portfolio_startdate = portfolio_startdate;
+	}
+	public void setPortfolio_enddate(String portfolio_enddate) {
+		this.portfolio_enddate = portfolio_enddate;
+	}
+	public void setPortfolio_participationrate(String portfolio_participationrate) {
+		this.portfolio_participationrate = portfolio_participationrate;
+	}
+	public void setPortfolio_thumbnailimg(String portfolio_thumbnailimg) {
+		this.portfolio_thumbnailimg = portfolio_thumbnailimg;
+	}
+}
