570fbb41827cabc9c3237958f6fb33ca7f872874
ktj <kye803112@gmail.com>
2020-09-05 15:09:41 +0900

    riskboard

---
+++ src/main/common/kr/or/ddit/vo/RiskJoinVO.java
@@ -0,0 +1,355 @@
+package kr.or.ddit.vo;
+
+public class RiskJoinVO {
+	
+	private String r;
+	private String risk_no;
+	private String project_no;
+	private String mem_id;
+	private String risk_title;
+	private String risk_content;
+	private String risk_regdate;
+	private String risk_group;
+	private String risk_seq;
+	private String risk_depth;
+	private String risk_hit;
+	private String project_processstatus;
+	private String project_processcategory;
+	private String project_consullting;
+	private String project_readystatus;
+	private String project_currentstatus;
+	private String project_businessinfo;
+	private String project_reference;
+	private String project_note;
+	private String project_technologies;
+	private String project_budget;
+	private String project_startdate;
+	private String project_duration;
+	private String project_endstatus;
+	private String project_premeeting;
+	private String project_proceedingmeeting;
+	private String project_meetingcycle;
+	private String project_clientlocation;
+	private String project_applicationdeadline;
+	private String project_supportstatus;
+	private String project_essentialrequirements;
+	private String project_manpower;
+	private String project_managementexperience;
+	private String project_futureplans;
+	private String project_priority;
+	private String project_enddate;
+	private String project_title;
+	private String project_clientinformation;
+	private String project_clientintroduce;
+	private String profile_seq;
+	private String profile_savename;
+	private String profile_realname;
+	private String profile_contenttype;
+	private String profile_size;
+	private String profile_regdate;
+	private String profile_downhit;
+	private String comment_content;
+	private String comment_regdate;
+	private String comment_seq;
+	
+	
+	public String getR() {
+		return r;
+	}
+	public void setR(String r) {
+		this.r = r;
+	}
+	public String getRisk_no() {
+		return risk_no;
+	}
+	public void setRisk_no(String risk_no) {
+		this.risk_no = risk_no;
+	}
+	public String getProject_no() {
+		return project_no;
+	}
+	public void setProject_no(String project_no) {
+		this.project_no = project_no;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getRisk_title() {
+		return risk_title;
+	}
+	public void setRisk_title(String risk_title) {
+		this.risk_title = risk_title;
+	}
+	public String getRisk_content() {
+		return risk_content;
+	}
+	public void setRisk_content(String risk_content) {
+		this.risk_content = risk_content;
+	}
+	public String getRisk_regdate() {
+		return risk_regdate;
+	}
+	public void setRisk_regdate(String risk_regdate) {
+		this.risk_regdate = risk_regdate;
+	}
+	public String getRisk_group() {
+		return risk_group;
+	}
+	public void setRisk_group(String risk_group) {
+		this.risk_group = risk_group;
+	}
+	public String getRisk_seq() {
+		return risk_seq;
+	}
+	public void setRisk_seq(String risk_seq) {
+		this.risk_seq = risk_seq;
+	}
+	public String getRisk_depth() {
+		return risk_depth;
+	}
+	public void setRisk_depth(String risk_depth) {
+		this.risk_depth = risk_depth;
+	}
+	public String getRisk_hit() {
+		return risk_hit;
+	}
+	public void setRisk_hit(String risk_hit) {
+		this.risk_hit = risk_hit;
+	}
+	public String getProject_processstatus() {
+		return project_processstatus;
+	}
+	public void setProject_processstatus(String project_processstatus) {
+		this.project_processstatus = project_processstatus;
+	}
+	public String getProject_processcategory() {
+		return project_processcategory;
+	}
+	public void setProject_processcategory(String project_processcategory) {
+		this.project_processcategory = project_processcategory;
+	}
+	public String getProject_consullting() {
+		return project_consullting;
+	}
+	public void setProject_consullting(String project_consullting) {
+		this.project_consullting = project_consullting;
+	}
+	public String getProject_readystatus() {
+		return project_readystatus;
+	}
+	public void setProject_readystatus(String project_readystatus) {
+		this.project_readystatus = project_readystatus;
+	}
+	public String getProject_currentstatus() {
+		return project_currentstatus;
+	}
+	public void setProject_currentstatus(String project_currentstatus) {
+		this.project_currentstatus = project_currentstatus;
+	}
+	public String getProject_businessinfo() {
+		return project_businessinfo;
+	}
+	public void setProject_businessinfo(String project_businessinfo) {
+		this.project_businessinfo = project_businessinfo;
+	}
+	public String getProject_reference() {
+		return project_reference;
+	}
+	public void setProject_reference(String project_reference) {
+		this.project_reference = project_reference;
+	}
+	public String getProject_note() {
+		return project_note;
+	}
+	public void setProject_note(String project_note) {
+		this.project_note = project_note;
+	}
+	public String getProject_technologies() {
+		return project_technologies;
+	}
+	public void setProject_technologies(String project_technologies) {
+		this.project_technologies = project_technologies;
+	}
+	public String getProject_budget() {
+		return project_budget;
+	}
+	public void setProject_budget(String project_budget) {
+		this.project_budget = project_budget;
+	}
+	public String getProject_startdate() {
+		return project_startdate;
+	}
+	public void setProject_startdate(String project_startdate) {
+		this.project_startdate = project_startdate;
+	}
+	public String getProject_duration() {
+		return project_duration;
+	}
+	public void setProject_duration(String project_duration) {
+		this.project_duration = project_duration;
+	}
+	public String getProject_endstatus() {
+		return project_endstatus;
+	}
+	public void setProject_endstatus(String project_endstatus) {
+		this.project_endstatus = project_endstatus;
+	}
+	public String getProject_premeeting() {
+		return project_premeeting;
+	}
+	public void setProject_premeeting(String project_premeeting) {
+		this.project_premeeting = project_premeeting;
+	}
+	public String getProject_proceedingmeeting() {
+		return project_proceedingmeeting;
+	}
+	public void setProject_proceedingmeeting(String project_proceedingmeeting) {
+		this.project_proceedingmeeting = project_proceedingmeeting;
+	}
+	public String getProject_meetingcycle() {
+		return project_meetingcycle;
+	}
+	public void setProject_meetingcycle(String project_meetingcycle) {
+		this.project_meetingcycle = project_meetingcycle;
+	}
+	public String getProject_clientlocation() {
+		return project_clientlocation;
+	}
+	public void setProject_clientlocation(String project_clientlocation) {
+		this.project_clientlocation = project_clientlocation;
+	}
+	public String getProject_applicationdeadline() {
+		return project_applicationdeadline;
+	}
+	public void setProject_applicationdeadline(String project_applicationdeadline) {
+		this.project_applicationdeadline = project_applicationdeadline;
+	}
+	public String getProject_supportstatus() {
+		return project_supportstatus;
+	}
+	public void setProject_supportstatus(String project_supportstatus) {
+		this.project_supportstatus = project_supportstatus;
+	}
+	public String getProject_essentialrequirements() {
+		return project_essentialrequirements;
+	}
+	public void setProject_essentialrequirements(
+			String project_essentialrequirements) {
+		this.project_essentialrequirements = project_essentialrequirements;
+	}
+	public String getProject_manpower() {
+		return project_manpower;
+	}
+	public void setProject_manpower(String project_manpower) {
+		this.project_manpower = project_manpower;
+	}
+	public String getProject_managementexperience() {
+		return project_managementexperience;
+	}
+	public void setProject_managementexperience(String project_managementexperience) {
+		this.project_managementexperience = project_managementexperience;
+	}
+	public String getProject_futureplans() {
+		return project_futureplans;
+	}
+	public void setProject_futureplans(String project_futureplans) {
+		this.project_futureplans = project_futureplans;
+	}
+	public String getProject_priority() {
+		return project_priority;
+	}
+	public void setProject_priority(String project_priority) {
+		this.project_priority = project_priority;
+	}
+	public String getProject_enddate() {
+		return project_enddate;
+	}
+	public void setProject_enddate(String project_enddate) {
+		this.project_enddate = project_enddate;
+	}
+	public String getProject_title() {
+		return project_title;
+	}
+	public void setProject_title(String project_title) {
+		this.project_title = project_title;
+	}
+	public String getProject_clientinformation() {
+		return project_clientinformation;
+	}
+	public void setProject_clientinformation(String project_clientinformation) {
+		this.project_clientinformation = project_clientinformation;
+	}
+	public String getProject_clientintroduce() {
+		return project_clientintroduce;
+	}
+	public void setProject_clientintroduce(String project_clientintroduce) {
+		this.project_clientintroduce = project_clientintroduce;
+	}
+	public String getProfile_seq() {
+		return profile_seq;
+	}
+	public void setProfile_seq(String profile_seq) {
+		this.profile_seq = profile_seq;
+	}
+	public String getProfile_savename() {
+		return profile_savename;
+	}
+	public void setProfile_savename(String profile_savename) {
+		this.profile_savename = profile_savename;
+	}
+	public String getProfile_realname() {
+		return profile_realname;
+	}
+	public void setProfile_realname(String profile_realname) {
+		this.profile_realname = profile_realname;
+	}
+	public String getProfile_contenttype() {
+		return profile_contenttype;
+	}
+	public void setProfile_contenttype(String profile_contenttype) {
+		this.profile_contenttype = profile_contenttype;
+	}
+	public String getProfile_size() {
+		return profile_size;
+	}
+	public void setProfile_size(String profile_size) {
+		this.profile_size = profile_size;
+	}
+	public String getProfile_regdate() {
+		return profile_regdate;
+	}
+	public void setProfile_regdate(String profile_regdate) {
+		this.profile_regdate = profile_regdate;
+	}
+	public String getProfile_downhit() {
+		return profile_downhit;
+	}
+	public void setProfile_downhit(String profile_downhit) {
+		this.profile_downhit = profile_downhit;
+	}
+	public String getComment_content() {
+		return comment_content;
+	}
+	public void setComment_content(String comment_content) {
+		this.comment_content = comment_content;
+	}
+	public String getComment_regdate() {
+		return comment_regdate;
+	}
+	public void setComment_regdate(String comment_regdate) {
+		this.comment_regdate = comment_regdate;
+	}
+	public String getComment_seq() {
+		return comment_seq;
+	}
+	public void setComment_seq(String comment_seq) {
+		this.comment_seq = comment_seq;
+	}
+	
+	
+	
+
+}
