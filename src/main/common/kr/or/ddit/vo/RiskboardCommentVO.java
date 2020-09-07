aa48c4d6992c0d01ee1dd23c1fc21be5f227ce36
DESKTOP-VJ1S1C0\KTJ <kye803112@gmail.com>
2020-09-07 09:40:44 +0900

    vo.추가

---
+++ src/main/common/kr/or/ddit/vo/RiskboardCommentVO.java
@@ -0,0 +1,45 @@
+package kr.or.ddit.vo;
+
+public class RiskboardCommentVO {
+	
+	private String risk_no;
+	private String mem_id;
+	private String comment_content;
+	private String comment_regdate;
+	private String comment_seq;
+	
+	
+	public String getRisk_no() {
+		return risk_no;
+	}
+	public void setRisk_no(String risk_no) {
+		this.risk_no = risk_no;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getComment_content() {
+		return comment_content;
+	}
+	public void setComment_content(String comment_content) {
+		this.comment_content = comment_content;
+	}
+	public String getComment_regdate() {
+		return comment_regdate;
+	}
+	public void setComment_regdate(String comment_regdate) {
+		this.comment_regdate = comment_regdate;
+	}
+	public String getComment_seq() {
+		return comment_seq;
+	}
+	public void setComment_seq(String comment_seq) {
+		this.comment_seq = comment_seq;
+	}
+	
+	
+
+}
